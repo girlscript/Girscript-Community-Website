d4cfe67390c8aebb0b16cd019b34272abcc40e98
codewithvk <vivekpatel7202@gmail.com>
2021-03-17 08:50:47 +0530

    Basic Structure modified

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,11 @@
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+// import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-   <Router>
-     <Navbar/>
-     <Switch>
-       <Route path='/'  exact/>
-     </Switch>
-     <Footer/>
-   </Router>
+    <>
+      <h1>App.js</h1>
+    </>
   );
 }
 
