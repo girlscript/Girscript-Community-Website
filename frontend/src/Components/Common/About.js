1fcb86a6bdb9700737d3f2aa6999c408f5d0b7f1
shreyanshi16 <53462278+shreyanshi16@users.noreply.github.com>
2021-03-27 19:40:05 +0530

    added about-us section

---
+++ frontend/src/Components/Common/About.js
@@ -0,0 +1,21 @@
+import Common from "./Common";
+
+const About = () => {
+  return (
+    <>
+      <Common
+        sectionheading="About us"
+        sectiontext="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+      do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      Ut enim ad minim veniam, quis nostrud exercitation ullamco
+      laboris nisi ut aliquip ex ea commodo consequat Duis aute
+      irure dolor in reprehenderit in voluptate velit"
+        visit="/"
+        btnname="Know more"
+        colour="#FFCE00"
+      />
+    </>
+  );
+};
+
+export default About;
--- frontend/src/Pages/Homepage.js
+++ frontend/src/Pages/Homepage.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import Mission from "../Components/Mission/Mission";
 import Testimonial from "../Components/Testimonial/Testimonial"
+import About from "../Components/Common/About"
+import Newsletter from "../Components/Common/Newsletter"
+import Join from "../Components/Common/Join"
 
 const HomePage = () => {
     return (
         <>
+            <About/>
             <Mission />
+            <Newsletter />
             <Testimonial />
+            <Join />
         </>
     )
 
