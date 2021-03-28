e484127d67db702907958c14732c9c2af5672207
unknown <ashutosh99october@gmail.com>
2021-03-28 22:02:05 +0530

    Added homepage v1.0

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,48 @@
-// import logo from './logo.svg';
+import image from './images/image1.png';
+import Card from 'react-bootstrap/Card';
+import {Button, Container} from 'react-bootstrap';
 import './App.css';
+import Jumbotron from 'react-bootstrap/Jumbotron'
 
 function App() {
   return (
     <>
-      <h1>App.js</h1>
+      <h1>
+        <br></br>
+      </h1>
+      <Card className="text-center">
+        <Card.Header></Card.Header>
+        <Card.Body>
+          <Card.Text className="image1-content">
+          <img  src={image} width="700" height="400" alt="logo"/>
+          <br></br>
+          <Jumbotron fluid >
+            <Container>
+            <div className="dashes dash-card">   
+              <br>
+              </br>
+              <br>
+              </br>
+              <br>
+              </br>
+            </div>
+            </Container>
+          </Jumbotron>
+          
+          </Card.Text>
+        </Card.Body>
+
+        <Jumbotron fluid >
+          <Container className="text-card">
+            <h1>HELP STUDENTS SUCCEED IN TECH</h1>
+            <br />
+            
+            <h2>We support newbies</h2>
+            <Button className="button-class">Join us</Button>
+          </Container>
+        </Jumbotron>
+
+      </Card>
     </>
   );
 }
