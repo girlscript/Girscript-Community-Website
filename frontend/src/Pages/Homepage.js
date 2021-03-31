64c9dca8cdcbbe2b0dde4d0179ec3e02810a1d7b
agarwalminks <61506798+agarwalminks@users.noreply.github.com>
2021-03-31 14:31:51 +0530

    made the navbar and footer

--- frontend/src/Pages/Homepage.js
+++ frontend/src/Pages/Homepage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Mission from "../Components/Mission/Mission";
 import Testimonial from "../Components/Testimonial/Testimonial"
+import Navbar from "../Components/Navbar/Navbar"
 import About from "../Components/Common/About"
 import Footer from "../Components/Footer/Footer"
 import Newsletter from "../Components/Common/Newsletter"
@@ -9,11 +10,11 @@ import Join from "../Components/Common/Join"
 const HomePage = () => {
     return (
         <>
+        <Navbar/>
             <About/>
             <Mission />
             <Newsletter />
             <Testimonial />
-            
             <Join />
             <Footer/>
         </>
