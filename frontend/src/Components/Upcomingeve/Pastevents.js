e9df3d293bb359bb5b6dd6adaeec88a0f147f49c
Rutuja Deshmukh <rutujadeshmukh295@gmail.com>
2021-04-20 20:22:10 +0530

    a file of dummy data added to test dats folder in src folder and modified code with use of map

--- frontend/src/Components/Upcomingeve/Pastevents.js
+++ frontend/src/Components/Upcomingeve/Pastevents.js
@@ -1,42 +1,15 @@
 import React from 'react';
 import "./Upcomingeve.css";
-import Card from 'react-bootstrap/Card';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import image4 from "../../images/event4.jpg";
-import image5 from "../../images/event5.jpg";
+import * as psteve from "../../Test_data/UpcomingEve";
 
 const Pastevents = () => {
     return(
         
             <Container>
             <Row>
-                <Col>
-                    <Card   id = 'upcomingeve' style={{ width: '18rem' }}  >
-                    <Card.Img variant="top" src= {image4}/>
-                    <Card.Body>
-                    <Card.Title>Web Desvlopment</Card.Title>
-                    <Card.Text>
-                        Some quick example text to build on the card title and make up the bulk of
-                        the card's content.
-                    </Card.Text>
-                    </Card.Body>
-                    </Card>
-                </Col>
-
-                <Col>
-                    <Card   id = 'upcomingeve' style={{ width: '18rem' }}  >
-                    <Card.Img variant="top" src= {image5}/>
-                    <Card.Body>
-                    <Card.Title>Hacktober Fest</Card.Title>
-                    <Card.Text>
-                        Some quick example text to build on the card title and make up the bulk of
-                        the card's content.
-                    </Card.Text>
-                    </Card.Body>
-                    </Card>
-                </Col>
+                {psteve.cards2}
 
             </Row>
             </Container>
--- frontend/src/Components/Upcomingeve/Upcomingevents.js
+++ frontend/src/Components/Upcomingeve/Upcomingevents.js
@@ -1,57 +1,15 @@
 import React from 'react';
 import "./Upcomingeve.css";
-import Card from 'react-bootstrap/Card';
+
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import image1 from "../../images/event1.jpeg";
-import image2 from "../../images/event2.jpeg";
-import image3 from "../../images/event3.jpg";
-
+import cards1 from "../../Test_data/UpcomingEve";
 const Upcomingevents = () => {
     return(
         
             <Container>
             <Row>
-                <Col>
-                    <Card   id = 'upcomingeve' style={{ width: '18rem' }}  >
-                    <Card.Img variant="top" src= {image1}/>
-                    <Card.Body>
-                    <Card.Title>Speed Designing with Canva</Card.Title>
-                    <Card.Text>
-                        Some quick example text to build on the card title and make up the bulk of
-                        the card's content.
-                    </Card.Text>
-                    </Card.Body>
-                    </Card>
-                </Col>
-
-                <Col>
-                    <Card   id = 'upcomingeve' style={{ width: '18rem' }}  >
-                    <Card.Img variant="top" src= {image2}/>
-                    <Card.Body>
-                    <Card.Title>Grtting Started with Github</Card.Title>
-                    <Card.Text>
-                        Some quick example text to build on the card title and make up the bulk of
-                        the card's content.
-                    </Card.Text>
-                    </Card.Body>
-                    </Card>
-                </Col>
-
-                <Col>
-                    <Card   id = 'upcomingeve' style={{ width: '18rem' }}  >
-                    <Card.Img variant="top" src= {image3}/>
-                    <Card.Body>
-                    <Card.Title>Getting Started with Graphs in java</Card.Title>
-                    <Card.Text>
-                        Some quick example text to build on the card title and make up the bulk of
-                        the card's content.
-                    </Card.Text>
-                    </Card.Body>
-                    </Card>
-                </Col>
-
+            {cards1} 
             </Row>
             </Container>
                 
