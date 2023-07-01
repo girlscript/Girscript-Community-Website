c6642816812ac8e0d199fa7344a237e54b00b981
Ratan Prajapati <ratanprajapati1242@gmail.com>
2023-07-01 15:56:46 +0530

    I added Nav icon Hover in navbar.css

--- frontend/src/Pages/Homepage.js
+++ frontend/src/Pages/Homepage.js
@@ -1,27 +1,27 @@
 import React from 'react';
 import Mission from "../Components/Mission/Mission";
-import Impact from "../Components/ourImpact/ourImpact"
-import Community from "../Components/ourCommunity/ourCommunity"
+import Impact from "../Components/OurImpact/OurImpact"
+import Community from "../Components/OurCommunity/OurCommunity"
 import Testimonial from "../Components/Testimonial/Testimonial"
 import About from "../Components/Common/About"
 import Newsletter from "../Components/Common/Newsletter"
 import Join from "../Components/Common/Join"
 import Navbar from "../Components/Navbar/Navbar"
 import ScrollTop from "../Components/ScrollTop/ScrollTop";
-
 import Upcomingeve from "../Components/Upcomingeve/Upcomingeve";
+
+
 
 const HomePage = () => {
     return (
         <>
-            <Navbar/>
-            
-            <About/>
+            < Navbar/>
+            <About />
             <Mission />
             <Impact />
-            <Community/>
+            <Community />
             <Newsletter />
-            <Upcomingeve/>
+            <Upcomingeve />
             <Testimonial />
             <Join />
             <ScrollTop />
